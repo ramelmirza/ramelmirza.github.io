8380ce1d56783223159c27cd18170a258c319aa8
Ramel Mirza <ramelmirza1@gmail.com>
2024-12-16 21:07:25 -0500

    updated for 2025

--- script.js
+++ script.js
@@ -4,37 +4,3 @@ function mode() {
     body.classList.toggle('dark-mode');
 
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    let height = window.innerHeight;
-
-    let ball = document.getElementById('ball');
-
-    let speed = 2.5;
-    let position = 0;
-    let isDown = true;
-
-    function movementBall() {
-        if(isDown === true) {
-            position += speed;
-
-            // Reaches the bottom of the page
-            if(position >= height - 72) {
-                isDown = false;
-            }
-        } else if (isDown === false) {
-            position -= speed;
-
-            if(position <= 0) {
-                isDown = true;
-            }
-        }
-    }
-
-    ball.style.top = `${position}px`;
-
-    requestAnimationFrame(movementBall);
-
-    movementBall();
-});
